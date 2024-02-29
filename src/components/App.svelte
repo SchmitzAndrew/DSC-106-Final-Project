b409ea9ae16bbf340fa4c3c351e2421c184dda3c
SchmitzAndrew <schmitzandrew03@gmail.com>
2024-02-29 15:02:25 -0800

    svelte starter code from shaokang

---
+++ src/components/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  // Write your JS here, or import other files
+</script>
+
+<main>
+  <h1>Svelte template</h1>
+
+  <p>Write your HTML here</p>
+</main>
+
+<style>
+  /* Write your CSS here */
+</style>
