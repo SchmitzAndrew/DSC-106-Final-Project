a2c94779f38d3d82de7244fa70d4ad22bcb40a02
SchmitzAndrew <schmitzandrew03@gmail.com>
2024-02-29 21:22:49 -0800

    add import statement for json files

--- src/components/ActualDistribution.svelte
+++ src/components/ActualDistribution.svelte
@@ -1,6 +1,8 @@
 <script>
   import * as d3 from 'd3';
   import { onMount } from 'svelte';
+  import centersData from './center.json';
+  import pointGuardsData from './point_guard.json';
 
   let svg;
 
