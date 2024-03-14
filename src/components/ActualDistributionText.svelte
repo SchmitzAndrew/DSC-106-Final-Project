af7d2e8b146c79fbd5742150eb7b3fee4e5e888c
hectoralonsogallo <95384261+hgallocodes@users.noreply.github.com>
2024-03-14 13:28:59 -0700

    Create ActualDistributionText.svelte

---
+++ src/components/ActualDistributionText.svelte
@@ -0,0 +1,16 @@
+<main>
+    <p>
+      Let's see how your prediction for the distributions of these two positions and their corresponding 
+      rebound percenatge compare to the actual distributions. Click on "Show Distribution" to reveal
+      the true distributions.
+    </p>
+
+  </main>
+  
+  <style>
+
+.text-content p {
+  font-size: 200px; /* Adjust the size as needed */
+  padding: 0 20px; /* Add some padding around the text */
+}
+  </style>
