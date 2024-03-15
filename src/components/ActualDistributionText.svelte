8da3a226ec4d6835e7018501aab8dac7591376ed
hectoralonsogallo <95384261+hgallocodes@users.noreply.github.com>
2024-03-14 20:22:33 -0700

    Update ActualDistributionText.svelte

--- src/components/ActualDistributionText.svelte
+++ src/components/ActualDistributionText.svelte
@@ -13,4 +13,10 @@
   font-size: 200px; /* Adjust the size as needed */
   padding: 0 20px; /* Add some padding around the text */
 }
+.controls {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 1em; /* Adjust this value to reduce space between controls and the plot */
+  }
   </style>
